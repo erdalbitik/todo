71b2174819624fcf66cf7d96793f3cc9cf7fab76
erdalbitik <erdalbitik@gmail.com>
2016-12-04 22:59:06 +0300

    remove unused import

--- src/main/java/com/ebitik/todo/config/SecurityConfig.java
+++ src/main/java/com/ebitik/todo/config/SecurityConfig.java
@@ -1,11 +1,8 @@
 package com.ebitik.todo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
-import org.springframework.security.authentication.encoding.MessageDigestPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
